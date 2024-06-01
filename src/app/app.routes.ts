3424789fb24950abcb06c0596fe5bc2309f96d00
Heinrich Haubold <haubold.heinrich@gmail.com>
2024-06-01 17:17:44 +0200

    Blank angular 18 app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { SettingsComponent } from './views/settings/settings.component';
-import { UserComponent } from './views/user/user.component';
 
-export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'user', component: UserComponent }
-];
+export const routes: Routes = [];
