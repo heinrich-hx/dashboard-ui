2d771a14ea283bc626199fe84a5f2576ceda1ce3
Heinrich Haubold <haubold.heinrich@gmail.com>
2024-06-01 17:08:25 +0200

    Adds services for config and auth

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * User authentication
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+}
